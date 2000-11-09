51e64bf8a8ef0078a6e388d3700be0cf84a2eac8
livshits <livshits@ba8beb34-e082-4677-a38a-25dc9495808b>
2000-11-09 19:51:38 +0000

    Replacing these by cpp files

--- trunk/src/lispevalhash.cc
+++
@@ -1,9 +0,0 @@
-
-#include "lispevalhash.h"
-
-void LispEvaluator::Evaluate(LispPtr& aResult,LispEnvironment& aEnvironment,
-                             LispPtr& aArguments)
-{
-    iCaller(aEnvironment,aResult,aArguments);
-}
-
