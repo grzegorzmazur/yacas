89aaa73c7e090642a92df6047b24856d83b37eab
ayalpinkus <ayalpinkus@ba8beb34-e082-4677-a38a-25dc9495808b>
2002-02-10 12:30:31 +0000

    Bug fix for Div/Rem with negative numbers, and added txt2yacasdoc.pl, and started an "essays" directory.

---
+++ trunk/manmake/txt2yacasdoc.pl
@@ -0,0 +1,124 @@
+#!/usr/bin/perl -w
+
+# convert plain ascii to Yacas manual code
+# Syntax of ascii files:
+# Paragraphs are separated by blank lines.
+# Chapter is triple Tab indented; Section is double Tab indented.
+# sample code is single Tab indented (note that linebreaks in sample code
+# must be also Tab indented or else you get two sample code blocks)
+# Ordinary text is not indented.
+# Itemized text is marked by * in the first position, followed by Tab
+# Enumerated text is marked by * followed by Tab, number and period.
+# Text surrounded by <i>...</i> becomes emphasized (must be on a single line!)
+# Text surrounded by {} becomes fixed-width (must not contain more than one set of {} inside!)
+# Use with care, test with manualmaker
+
+$have_Text = 0;	# Means we have a Text() declared somewhere above
+$have_par = 1;	# Means we already have a new paragraph
+$in_text = 0;	# Means we are inside quotes of the Text()
+$in_itemized = 0;
+$in_enum = 0;
+$in_htmlcommand = 0;
+
+while (<STDIN>) {
+	chomp;
+	s/"/\\"/g;
+	
+	if (/^\t\t\t(.*)$/) {	# Chapter
+		&finish_text();
+		print "\tChapter()\"", &escape_term($1), "\";\n\n";
+		$have_par = 1;
+	} elsif (/^\t\t(.*)$/) {	# Section
+		&finish_text();
+		print "\tSection()\"", &escape_term($1), "\";\n\n";
+		$have_par = 1;
+	} elsif (/^\t(.*)$/) {	# Sample code
+		if (not $in_htmlcommand) {
+			&close_quote();
+			print ":HtmlCommand(\"\n";
+			$in_htmlcommand = 1;
+		}
+		print "$1\n";
+		$have_par = 0;
+		$in_text = 1;
+	} elsif (/^\s*$/) {	# New paragraph
+		if ($in_itemized or $in_enum) {
+			&close_quote();
+			print ")\n";
+			$in_itemized = $in_enum = 0;
+		}
+		if (not $have_par) {
+			&close_quote();
+			print ":HtmlNewParagraph()\n\n";
+			$have_par = 1;
+		}
+	} elsif (/^\*\t[0-9]+\. (.*)$/) {	# Enum
+		&close_quote();
+		if ($in_enum) {
+			print ":Item()\"", &escape_term($1), "\n";
+		} else {
+			print ":Enumerate() (\nItem()\"", &escape_term($1), "\n";
+			$in_enum = 1;
+		}
+		$in_text = 1;
+		$have_par = 0;
+	} elsif (/^\*\t(.*)$/) {	# Itemized
+		&close_quote();
+		if ($in_itemized) {
+			print ":Item()\"", &escape_term($1), "\n";
+		} else {
+			print ":Enumerate() (\nItem()\"", &escape_term($1), "\n";
+			$in_itemized = 1;
+		}
+		$in_text = 1;
+		$have_par = 0;
+	} else {	# plain text
+		if (not $have_Text) {
+			print "Text()\"";
+			$have_Text = $in_text = 1;
+		}
+		&open_quote();
+		print &escape_term($_), "\n";
+		$have_par = 0;
+		$in_text = 1;
+	}
+	
+}
+
+# Finish
+
+&finish_text();
+
+sub close_quote {
+	if ($in_text) {
+		print "\"" ;
+	}
+	if ($in_htmlcommand) {
+		print ")";
+	}
+	$in_htmlcommand = $in_text = 0;
+}
+sub open_quote {
+	if (not $in_text) {
+		print ":\"" ;
+		$in_text = 1;
+	}
+}
+
+sub finish_text {
+	&close_quote();
+	if ($in_itemized or $in_enum) {
+		print "\n);" ;
+		$in_itemized = $in_enum = 0;
+	} else {
+		print ";" if ($have_Text);
+	}
+	$have_Text = 0;
+}
+
+sub escape_term {
+	my ($text) = @_;
+	$text =~ s/\{((?:[^{}]*(?:\{[^{}]+\})*)+)\}/":HtmlTerm("$1"):"/g;
+	$text =~ s/<i>(.*)<\/i>/":HtmlEmphasis("$1"):"/gi;
+	$text;
+}
