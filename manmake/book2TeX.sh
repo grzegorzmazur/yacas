78ea1a63a6a8d78fcfbd85e8e23862b927e3f19f
Serge Winitzki <winitzki@gmail.com>
2002-02-14 04:59:20 +0000

    changes to generate LaTeX, PS, PDF docs for intro and coding books

--- manmake/book2TeX.sh
+++ manmake/book2TeX.sh
@@ -6,18 +6,23 @@
 
 if [ x"$1" = x ]; then
 	echo "Convert Yacas docs to TeX"
-	echo "Usage: `basename $0` bookname.chapt"
+	echo "Usage: `basename $0` bookname.chapt [outputname.tex]"
 else
+if [ x"$2" = x ]; then
+	target="$1.tex"
+else
+	target="$2"
+fi
 ourdir=`dirname $0`
 if [ -r "$1" ]; then
-	rm -f "$1.tex"
-	echo 'ToFile("'"$1"'.tex") [ Use("'"$ourdir"'/book2TeX.ys"); Load("'"$1"'"); TeXFinishUp(); ];' | yacas -f
-	if [ -s "$1.tex" ]; then
-		echo "File '$1.tex' was created."
-		perl -e 'while(<>) { s/([%])/\\$1/g; print; }' < "$1.tex" > "$1.tex.tmp"
-		mv "$1.tex.tmp" "$1.tex"
+	rm -f "$target"
+	echo 'ToFile("'"$target"'") [ Use("'"$ourdir"'/book2TeX.ys"); Load("'"$1"'"); TeXFinishUp(); ];' | yacas -f
+	if [ -s "$target" ]; then
+		echo "File '$target' was created."
+		#perl -e 'while(<>) { s/([%])/\\$1/g; print; }' < "$target" > "$target.tmp"
+		#mv "$target.tmp" "$target"
 	else
-		echo "book2TeX: Some problem generating file '$1.tex', aborted."
+		echo "book2TeX: Some problem generating file '$target', aborted."
 	fi
 else
 	echo "Error: cannot read input file '$1'."
