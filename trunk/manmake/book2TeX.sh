6626108a73085bac6383959007ae91d1053b3b1f
winitzki <winitzki@ba8beb34-e082-4677-a38a-25dc9495808b>
2002-04-30 16:52:18 +0000

    small changes related to documentation: 1) used *EVAL to insert live Yacas version into docs (wester-1994 and algorithms-specfunc) 2) wrote some documentation on how to debug errors in the manual 3) fixed sh scripts: added -run option to use another yacas executable 4) tried to prepare a cleaner "make distcheck"; current version works but it overrides the automatic "distcheck" generated by automake, hence warning messages. Added $(srcdir) and $(top_srcdir) so that texdocs can be generated outside of the main tree if necessary.

--- trunk/manmake/book2TeX.sh
+++ trunk/manmake/book2TeX.sh
@@ -4,17 +4,25 @@
 # Usage: book2txt.sh bookname.chapt
 # result: bookname.chapt.tex
 
+ourdir=`dirname $0`
+
+if [ x"$1" = x-run ]; then
+	shift
+	yacas="$1"
+	shift
+else
+	yacas="$ourdir/../src/yacas --rootdir $ourdir/../scripts/"
+fi
+
 if [ x"$1" = x ]; then
-	echo "Convert Yacas docs to TeX"
-	echo "Usage: `basename $0` bookname.chapt [outputname.tex]"
+	echo "`basename $0`: convert Yacas docs to TeX"
+	echo "Usage: `basename $0` [-run /path/yacas] bookname.chapt [outputname.tex]"
 else
 if [ x"$2" = x ]; then
 	target="$1.tex"
 else
 	target="$2"
 fi
-ourdir=`dirname $0`
-yacas="$ourdir/../src/yacas --rootdir $ourdir/../scripts/"
 
 if [ -r "$1" ]; then
 	rm -f "$target"
@@ -26,7 +34,7 @@ if [ -r "$1" ]; then
 		perl -e '$out_of_verbatim=1; while(<>) { if($out_of_verbatim) { 1 while s/^([^{}|]*(?:\{[^{}]*\}[^{}|]*|\\verb\|[^|]*\|[^{}|]*)*[^{}|]*[- ()|\[\]\t])"/$1``/g; s/^"/``/; if (/\\begin\{verbatim\}/) {$out_of_verbatim=0; } } elsif(/\\end\{verbatim\}/) { $out_of_verbatim=1 }; print; }' < "$target" > "$target.tmp"
 		mv "$target.tmp" "$target"
 	else
-		echo "book2TeX: Some problem generating file '$target', aborted."
+		echo "`basename $0`: Some problem generating file '$target', aborted."
 	fi
 else
 	echo "Error: cannot read input file '$1'."
