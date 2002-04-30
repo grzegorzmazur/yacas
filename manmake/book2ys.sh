15528bb34da640eaf88c2df596893dbfd669cea7
Serge Winitzki <winitzki@gmail.com>
2002-04-30 16:52:18 +0000

    small changes related to documentation: 1) used *EVAL to insert live Yacas version into docs (wester-1994 and algorithms-specfunc) 2) wrote some documentation on how to debug errors in the manual 3) fixed sh scripts: added -run option to use another yacas executable 4) tried to prepare a cleaner "make distcheck"; current version works but it overrides the automatic "distcheck" generated by automake, hence warning messages. Added $(srcdir) and $(top_srcdir) so that texdocs can be generated outside of the main tree if necessary.

--- manmake/book2ys.sh
+++ manmake/book2ys.sh
@@ -4,17 +4,25 @@
 # Usage: book2tys.sh bookname.chapt
 # result: bookname.chapt.ys
 
+ourdir=`dirname $0`
+
+if [ x"$1" = x-run ]; then
+	shift
+	yacas="$1"
+	shift
+else
+	yacas="$ourdir/../src/yacas --rootdir $ourdir/../scripts/"
+fi
+
 if [ x"$1" = x ]; then
-	echo "Extract code from Yacas docs"
-	echo "Usage: `basename $0` bookname.chapt [outputname.ys]"
+	echo "`basename $0`: extract code from Yacas docs"
+	echo "Usage: `basename $0` [-run /path/yacas] bookname.chapt [outputname.ys]"
 else
 if [ x"$2" = x ]; then
 	target="$1.ys"
 else
 	target="$2"
 fi
-ourdir=`dirname $0`
-yacas="$ourdir/../src/yacas --rootdir $ourdir/../scripts/"
 
 if [ -r "$1" ]; then
 	rm -f "$target"
@@ -24,7 +32,7 @@ if [ -r "$1" ]; then
 		#perl -e 'while(<>) { s/([%])/\\$1/g; print; }' < "$target" > "$target.tmp"
 		#mv "$target.tmp" "$target"
 	else
-		echo "book2ys: Some problem generating file '$target', aborted."
+		echo "`basename $0`: Some problem generating file '$target', aborted."
 	fi
 else
 	echo "Error: cannot read input file '$1'."
