ae58907581da6c7d4aee880c404f5d08c11c0c5f
winitzki <winitzki@ba8beb34-e082-4677-a38a-25dc9495808b>
2002-10-23 14:58:56 +0000

    small feature changes: - added the *YSFILE label for literate programming, - corrected a bug with yacasdoc:// references with anchored text.

--- trunk/manmake/book2ys.sh
+++ trunk/manmake/book2ys.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Convert Yacas books to Yacas code
-# Usage: book2tys.sh bookname.chapt
-# result: bookname.chapt.ys
+# Convert Yacas books to Yacas code, taking only code examples that don't begin with In> or Out>.
+# All other lines become comments unless the -strip option is given.
 
 ourdir=`dirname $0`
 
@@ -14,9 +13,16 @@ else
 	yacas="$ourdir/../src/yacas --rootdir $ourdir/../scripts/"
 fi
 
+if [ x"$1" = x-strip ]; then
+	shift
+	strip="True"
+else
+	strip="False"
+fi
+
 if [ x"$1" = x ]; then
 	echo "`basename $0`: extract code from Yacas docs"
-	echo "Usage: `basename $0` [-run /path/yacas] bookname.chapt [outputname.ys]"
+	echo "Usage: `basename $0` [-run /path/yacas] [-strip] bookname.chapt [outputname.ys]"
 else
 if [ x"$2" = x ]; then
 	target="$1.ys"
@@ -26,7 +32,7 @@ fi
 
 if [ -r "$1" ]; then
 	rm -f "$target"
-	echo 'ToFile("'"$target"'") [ Use("'"$ourdir"'/book2ys.ys"); Load("'"$1"'"); ];' | $yacas -f
+	echo "ToFile(\"$target\") [ Use(\"$ourdir/book2ys.ys\"); StripComments := $strip; Load(\"$1\"); ];" | $yacas -f
 	if [ -s "$target" ]; then
 		echo "File '$target' was created."
 		#perl -e 'while(<>) { s/([%])/\\$1/g; print; }' < "$target" > "$target.tmp"
