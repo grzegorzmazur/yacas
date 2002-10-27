a1a0562e69451636a3c3f7a8b67b8b774ae77952
ayalpinkus <ayalpinkus@ba8beb34-e082-4677-a38a-25dc9495808b>
2002-10-27 15:48:48 +0000

    Initial version of the numeric class abstracting away the numeric library used.

--- trunk/src/numbers.h
+++ trunk/src/numbers.h
@@ -87,6 +87,106 @@ LispStringPtr BitXor(LispCharPtr int1, LispCharPtr int2,
 LispStringPtr LispFactorial(LispCharPtr int1, LispHashTable& aHashTable,LispInt aPrecision);
 
 
+
+/** Base number class. It is recommended that the derived classes add the following 
+ *  static method to construct a number type:
+ *
+ *  static YacasBigNumber* Make(LispCharPtr aString,LispInt aPrecision,LispInt aBase=10);
+ *
+ */
+ 
+// reference-counting through a smart pointer, so we can do automatic clean up on numbers
+class YacasBigNumber;
+typedef RefPtr<YacasBigNumber> YacasBigNumberPtr;
+// pure abstract class representing a number
+class YacasBigNumber : public RefCountedObject
+{
+public: //constructors
+  /// Copy a number class
+  virtual const YacasBigNumber* Copy() const = 0;
+  /// ToString : return string representation of number in aResult 
+  virtual void ToString(LispString& aResult, LispInt aBase) = 0;
+public: //base conversions
+  virtual YacasBigNumber* FromBase( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,  LispInt aPrecision) = 0;
+  virtual YacasBigNumber* ToBase( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,  LispInt aPrecision) = 0;
+public: //information retrieval on library used  
+  /// Whether the numeric library supports 1.0E-10 and such.
+  virtual LispInt NumericSupportForMantissa() = 0;
+  /// Numeric library name
+  virtual const LispCharPtr NumericLibraryName() = 0;
+  
+public://arithmetic
+  /// Calculate GCD of this object with another, and return result in aResult
+  virtual YacasBigNumber* Gcd(YacasBigNumberPtr aX, YacasBigNumberPtr aY) = 0;
+  /// Multiply two numbers, and return result in aResult
+  virtual YacasBigNumber* Multiply(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  /** Multiply two numbers, and add to aResult (this is useful and generally efficient to implement).
+   * This is most likely going to be used by internal functions only, using aResult as an accumulator.
+   */
+  virtual YacasBigNumber* MultiplyAdd(YacasBigNumber* aResult,
+                const YacasBigNumberPtr& aX, 
+                const YacasBigNumberPtr& aY, 
+                LispInt aPrecision) = 0;
+  /// Add two numbers, and return result in aResult
+  virtual YacasBigNumber* Add(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  /// Negate the current number, and return it
+  virtual YacasBigNumber* Negate() = 0;
+  /// Divide, and return result  
+  virtual YacasBigNumber* Divide(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  /// Raise power, and return result
+  virtual YacasBigNumber* Power(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,LispInt aPrecision) = 0;
+public: //trigonometric functions
+  virtual YacasBigNumber* Sin(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Cos(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Tan(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* ArcSin(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* ArcCos(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* ArcTan(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Exp(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Ln(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+public://other functions  
+  virtual YacasBigNumber* Sqrt(const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Abs( const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Factorial(LispCharPtr int1, LispHashTable& aHashTable,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Floor(const YacasBigNumberPtr& aX, LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Ceil( const YacasBigNumberPtr& aX,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Mod( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Div( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  virtual YacasBigNumber* Pi(LispInt aPrecision) = 0;
+public://comparisons  
+  virtual LispBoolean GreaterThan(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  virtual LispBoolean LessThan(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+
+public://bitwise operations  
+  virtual YacasBigNumber* ShiftLeft( LispCharPtr int1, LispCharPtr int2, LispHashTable& aHashTable,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* ShiftRight( LispCharPtr int1, LispCharPtr int2, LispHashTable& aHashTable,LispInt aPrecision) = 0;
+  virtual YacasBigNumber* BitAnd(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  virtual YacasBigNumber* BitOr(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+  virtual YacasBigNumber* BitXor(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) = 0;
+
+protected: // I don't want any one to construct this class any other way!
+  YacasBigNumber(){};
+
+
+};
+
+
+
+
+
+
+/*
+YacasBigNumberPtr adder = MakeBigNum();
+YacasBigNumberPtr result =  adder->Add(
+                              MakeBigNum("23",10),
+                              MakeBigNum("45.23e3",10)
+                                      );
+  LispString str;
+  result->ToString(str,10);
+  aResult.Set(LispAtom::New(iEnvironment.HashTable().Lookup(str.String())));
+*/
+
+
 #endif
 
 
--- trunk/src/platmath.cpp
+++ trunk/src/platmath.cpp
@@ -6,6 +6,7 @@
 #include "lispobject.h"
 #include "lispatom.h"
 #include "lispenvironment.h"
+#include "numbers.h"
 
 
 double GetDouble(LispCharPtr aString)
@@ -100,3 +101,347 @@ LispStringPtr PlatAbs(LispCharPtr int1, LispHashTable& aHashTable,LispInt aPreci
 
 
 
+
+
+
+
+
+
+/* And here is a reference implementation of YacasBigNumber: one using native types.
+*/
+
+#define NATIVE_CAST(x)  ((const YacasNativeNumber*)(x.Ptr()))
+
+class YacasNativeNumber : public YacasBigNumber
+{
+public: //constructors
+  /// Copy a number class
+  static YacasNativeNumber* Make(LispCharPtr aString,LispInt aPrecision,LispInt aBase=10);
+  virtual YacasBigNumber* Copy() const;
+  /// ToString : return string representation of number in aResult 
+  virtual void ToString(LispString& aResult, LispInt aBase) ;
+public: //base conversions
+  virtual YacasBigNumber* FromBase( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,  LispInt aPrecision) ;
+  virtual YacasBigNumber* ToBase( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,  LispInt aPrecision) ;
+public: //information retrieval on library used  
+  /// Whether the numeric library supports 1.0E-10 and such.
+  virtual LispInt NumericSupportForMantissa() ;
+  /// Numeric library name
+  virtual const LispCharPtr NumericLibraryName() ;
+  
+public://arithmetic
+  /// Calculate GCD of this object with another, and return result in aResult
+  virtual YacasBigNumber* Gcd(YacasBigNumberPtr aX, YacasBigNumberPtr aY) ;
+  /// Multiply two numbers, and return result in aResult
+  virtual YacasBigNumber* Multiply(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  /** Multiply two numbers, and add to aResult (this is useful and generally efficient to implement).
+   * This is most likely going to be used by internal functions only, using aResult as an accumulator.
+   */
+  virtual YacasBigNumber* MultiplyAdd(YacasBigNumber* aResult,
+                const YacasBigNumberPtr& aX, 
+                const YacasBigNumberPtr& aY, 
+                LispInt aPrecision) ;
+  /// Add two numbers, and return result in aResult
+  virtual YacasBigNumber* Add(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  /// Negate the current number, and return it
+  virtual YacasBigNumber* Negate() ;
+  /// Divide, and return result  
+  virtual YacasBigNumber* Divide(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  /// Raise power, and return result
+  virtual YacasBigNumber* Power(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,LispInt aPrecision) ;
+public: //trigonometric functions
+  virtual YacasBigNumber* Sin(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* Cos(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* Tan(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* ArcSin(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* ArcCos(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* ArcTan(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* Exp(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* Ln(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+public://other functions  
+  virtual YacasBigNumber* Sqrt(const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* Abs( const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* Factorial(LispCharPtr int1, LispHashTable& aHashTable,LispInt aPrecision) ;
+  virtual YacasBigNumber* Floor(const YacasBigNumberPtr& aX, LispInt aPrecision) ;
+  virtual YacasBigNumber* Ceil( const YacasBigNumberPtr& aX,LispInt aPrecision) ;
+  virtual YacasBigNumber* Mod( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  virtual YacasBigNumber* Div( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  virtual YacasBigNumber* Pi(LispInt aPrecision) ;
+public://comparisons  
+  virtual LispBoolean GreaterThan(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  virtual LispBoolean LessThan(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+
+public://bitwise operations  
+  virtual YacasBigNumber* ShiftLeft( LispCharPtr int1, LispCharPtr int2, LispHashTable& aHashTable,LispInt aPrecision) ;
+  virtual YacasBigNumber* ShiftRight( LispCharPtr int1, LispCharPtr int2, LispHashTable& aHashTable,LispInt aPrecision) ;
+  virtual YacasBigNumber* BitAnd(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  virtual YacasBigNumber* BitOr(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+  virtual YacasBigNumber* BitXor(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) ;
+
+private:
+  inline YacasNativeNumber() {type = KDouble;value.d = 0.0;}
+  inline YacasNativeNumber(const YacasNativeNumber& aOther)
+  {
+    type  = aOther.type;
+    value = aOther.value;
+  }
+  inline YacasNativeNumber(const YacasBigNumberPtr& aOther)
+  {
+    type  = NATIVE_CAST(aOther)->type;
+    value = NATIVE_CAST(aOther)->value;
+  }
+  enum EType
+  {
+    KInteger = 0,
+    KDouble=1,
+  };
+  union UValue
+  {
+    long i;
+    double  d;
+  };
+  EType type;
+  UValue value;
+};
+
+const YacasBigNumber* MakeNativeNumber(LispCharPtr aString,LispInt aPrecision,LispInt aBase)
+{
+  return YacasNativeNumber::Make(aString,aPrecision,aBase);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+YacasNativeNumber(const YacasBigNumberPtr& aPtr)
+{
+}
+*/
+
+
+
+
+
+YacasNativeNumber* YacasNativeNumber::Make(LispCharPtr aString,LispInt aPrecision,LispInt aBase=10)
+{
+  YacasNativeNumber* result = NEW YacasNativeNumber();
+  result->type  = KDouble;
+  result->value.d = GetDouble(aString);
+  return result;
+}
+YacasBigNumber* YacasNativeNumber::Copy() const
+{
+  YacasNativeNumber* result = NEW YacasNativeNumber();
+  result->type  = type;
+  result->value = value;
+  return result;
+}
+void YacasNativeNumber::ToString(LispString& aResult, LispInt aBase) 
+{
+  char dummy[150];
+  dummy[0] = '\0';
+  switch (type)
+  {
+  case KInteger:
+    sprintf(dummy,"%ld",value.i);
+    break;
+  case KDouble:
+    sprintf(dummy,"%f",value.d);
+    break;
+  }
+  aResult.SetStringCounted(dummy,PlatStrLen(dummy));
+}
+YacasBigNumber* YacasNativeNumber::FromBase( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,  LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::ToBase( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,  LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+LispInt YacasNativeNumber:: NumericSupportForMantissa() 
+{
+  return LispTrue;
+}
+const LispCharPtr YacasNativeNumber::NumericLibraryName() 
+{
+  return "Native types";
+}
+
+YacasBigNumber* YacasNativeNumber::Gcd(YacasBigNumberPtr aX, YacasBigNumberPtr aY) 
+{
+  return NULL;//TODO
+}
+
+YacasBigNumber* YacasNativeNumber::Multiply(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  LISPASSERT(!StrCompare(aX.NumericLibraryName(), aY.NumericLibraryName()));
+  YacasNativeNumber* result = NEW YacasNativeNumber();
+
+
+  switch (NATIVE_CAST(aX)->type)
+  {
+    case KInteger:
+      switch (NATIVE_CAST(aY)->type)
+      {
+        case KInteger:
+          result->type = KInteger;
+          result->value.i = ((YacasNativeNumber)aX).value.i * ((YacasNativeNumber)aY).value.i;
+          break;
+        case KDouble:
+          result->type = KDouble;
+          result->value.d = ((YacasNativeNumber)aX).value.i * ((YacasNativeNumber)aY).value.d;
+          break;
+      }
+      break;
+
+    case KDouble:
+      switch (NATIVE_CAST(aY)->type)
+      {
+        case KInteger:
+          result->type = KDouble;
+          result->value.d = ((YacasNativeNumber)aX).value.d * ((YacasNativeNumber)aY).value.i;
+          break;
+        case KDouble:
+          result->type = KDouble;
+          result->value.d = ((YacasNativeNumber)aX).value.d * ((YacasNativeNumber)aY).value.d;
+          break;
+      }
+      break;
+  }
+  return result;
+}
+
+YacasBigNumber* YacasNativeNumber::MultiplyAdd(YacasBigNumber* aResult,
+              const YacasBigNumberPtr& aX, 
+              const YacasBigNumberPtr& aY, 
+              LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Add(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Negate() 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Divide(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Power(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY,LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+
+#define ONE_FUN(fn)                                                   \
+  {                                                                   \
+    YacasNativeNumber* result = NEW YacasNativeNumber();              \
+    result->type = KDouble;                                           \
+    double dvalue = 0;                                                \
+    switch (NATIVE_CAST(aX)->type)                                    \
+    {                                                                 \
+      case KInteger: dvalue = NATIVE_CAST(aX)->value.i; break;        \
+      case KDouble:  dvalue = NATIVE_CAST(aX)->value.d; break;        \
+    }                                                                 \
+    result->value.d = fn(dvalue);                                    \
+    return result;                                                    \
+  }
+
+
+YacasBigNumber* YacasNativeNumber::Sin(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(sin)
+
+YacasBigNumber* YacasNativeNumber::Cos(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(cos)
+
+YacasBigNumber* YacasNativeNumber::Tan(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(tan)
+
+YacasBigNumber* YacasNativeNumber::ArcSin(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(asin)
+
+YacasBigNumber* YacasNativeNumber::ArcCos(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(acos)
+
+YacasBigNumber* YacasNativeNumber::ArcTan(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(atan)
+
+YacasBigNumber* YacasNativeNumber::Exp(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(exp)
+
+YacasBigNumber* YacasNativeNumber::Ln(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+ONE_FUN(log)
+
+YacasBigNumber* YacasNativeNumber::Sqrt(const YacasBigNumberPtr& aX,LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Abs( const YacasBigNumberPtr& aX,LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Factorial(LispCharPtr int1, LispHashTable& aHashTable,LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Floor(const YacasBigNumberPtr& aX, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Ceil( const YacasBigNumberPtr& aX,LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Mod( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Div( const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::Pi(LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+LispBoolean YacasNativeNumber::GreaterThan(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+LispBoolean YacasNativeNumber::LessThan(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::ShiftLeft( LispCharPtr int1, LispCharPtr int2, LispHashTable& aHashTable,LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::ShiftRight( LispCharPtr int1, LispCharPtr int2, LispHashTable& aHashTable,LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::BitAnd(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::BitOr(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
+YacasBigNumber* YacasNativeNumber::BitXor(const YacasBigNumberPtr& aX, const YacasBigNumberPtr& aY, LispInt aPrecision) 
+{
+  return NULL;//TODO
+}
