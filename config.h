75a268d8dfcd0e46ae7608488bbbc3e90881b0d8
Ayal Pinkus <apinkus@xs4all.nl>
2002-10-28 21:13:38 +0000

    More small fixes, as suggested by Frank.

--- config.h
+++ config.h
@@ -54,6 +54,9 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
+/* Define to 1 if you have the `strchr' function. */
+#define HAVE_STRCHR 1
+
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
 
