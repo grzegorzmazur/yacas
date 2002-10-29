73bb0386e7762f8cd4009f7316d83f0c19fff350
Ayal Pinkus <apinkus@xs4all.nl>
2002-10-29 23:48:31 +0000

    New trial for design of numeric class.

--- src/platmath.h
+++ src/platmath.h
@@ -27,8 +27,68 @@ LispStringPtr PlatDiv(LispCharPtr int1, LispCharPtr int2,LispHashTable& aHashTab
 LispStringPtr PlatAbs(LispCharPtr int1, LispHashTable& aHashTable,LispInt aPrecision);
 
 
-class YacasNativeNumber;
-YacasBigNumber* MakeNativeNumber(LispCharPtr aString,LispInt aPrecision,LispInt aBase=10);
+class NativeNumber : public NumberBase
+{
+public: //constructors
+  /// Construct a number class
+  NativeNumber(LispCharPtr aString,LispInt aPrecision,LispInt aBase=10);
+public: //constructors
+  /// ToString : return string representation of number in aResult 
+  virtual void ToString(LispString& aResult, LispInt aBase);
+public: //information retrieval on library used  
+  /// Numeric library name
+  virtual const LispCharPtr NumericLibraryName();
+public://arithmetic
+  /// Multiply two numbers, and return result in aResult
+  virtual void Multiply(const NumberBase& aX, const NumberBase& aY, LispInt aPrecision);
+  /** Multiply two numbers, and add to aResult (this is useful and generally efficient to implement).
+   * This is most likely going to be used by internal functions only, using aResult as an accumulator.
+   */
+  virtual void MultiplyAdd(NumberBase& aResult,
+                const NumberBase& aX, 
+                const NumberBase& aY, 
+                LispInt aPrecision);
+  /// Add two numbers, and return result in aResult
+  virtual void Add(const NumberBase& aX, const NumberBase& aY, LispInt aPrecision);
+  /// Negate the current number, and return it
+  virtual void Negate(const NumberBase& aX);
+  /// Divide, and return result. Note: if the two arguments are integer, it should return an integer result!
+  virtual void Divide(const NumberBase& aX, const NumberBase& aY, LispInt aPrecision);
+
+public://bitwise operations  
+  virtual void ShiftLeft( const NumberBase& aX, LispInt aNrToShift);
+  virtual void ShiftRight( const NumberBase& aX, LispInt aNrToShift);
+  virtual void BitAnd(const NumberBase& aX, const NumberBase& aY);
+  virtual void BitOr(const NumberBase& aX, const NumberBase& aY);
+  virtual void BitXor(const NumberBase& aX, const NumberBase& aY);
+
+public:
+  inline NativeNumber(const NativeNumber& aOther)
+  {
+    type  = aOther.type;
+    value = aOther.value;
+  }
+  inline NativeNumber(const NumberBase& aOther)
+  {
+    type  = ((const NativeNumber*)(&aOther))->type;
+    value = ((const NativeNumber*)(&aOther))->value;
+  }
+
+  inline NativeNumber() {type = KDouble;value.d = 0.0;}
+private:
+  enum EType
+  {
+    KInteger = 0,
+    KDouble=1,
+  };
+  union UValue
+  {
+    long i;
+    double  d;
+  };
+  EType type;
+  UValue value;
+};
 
 
 #endif
