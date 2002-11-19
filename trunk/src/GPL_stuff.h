b32b573bf0d58c362e1f1c8694735669c433fd53
winitzki <winitzki@ba8beb34-e082-4677-a38a-25dc9495808b>
2002-11-19 00:50:31 +0000

    1) updated changelog 2) added GPL blurb definitions in a separate header file for convenience

---
+++ trunk/src/GPL_stuff.h
@@ -0,0 +1,20 @@
+#ifndef __GPL_stuff_h__
+#define __GPL_stuff_h__
+
+// This is the full text for systems where the online help (?blah) is available
+#define GPL_blurb \
+"Yacas comes with ABSOLUTELY NO WARRANTY; type ?warranty for full details.\n" \
+"Yacas is Free Software--Free as in Freedom--so you can redistribute Yacas\n" \
+"and modify it; type ?license or ?licence to see the full conditions\n" \
+"of the GNU General Public License (GPL).\n" \
+"\n"
+
+// This is for systems where online help (?blah) is normally not available
+#define GPL_blurb_nohelp \
+"Yacas is Free Software--Free as in Freedom--so you can redistribute Yacas\n" \
+"and modify it; see the GNU General Public License for the full conditions.\n" \
+"Yacas comes with ABSOLUTELY NO WARRANTY, as specified in the GPL.\n" \
+"\n"
+
+
+#endif
