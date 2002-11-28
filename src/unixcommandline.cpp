f74cdbd239d754a5937b04638f3fafe89cc2eb44
Serge Winitzki <winitzki@gmail.com>
2002-11-28 15:24:46 +0000

    removed a warning due to type of clock()

--- src/unixcommandline.cpp
+++ src/unixcommandline.cpp
@@ -12,7 +12,7 @@ void CUnixCommandLine::NewLine()
 
 void CUnixCommandLine::Pause()
 {
-    unsigned int i=clock()+CLOCKS_PER_SEC/4;
+    clock_t i=clock()+CLOCKS_PER_SEC/4;
     while (clock()<i);
 }
 
