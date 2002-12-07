7e24b6be4b70835d48242819d60c7394e0bb1fce
Jitse Niesen <j.niesen@leeds.ac.uk>
2002-12-07 00:45:44 +0000

    Added autotools support to the embed/ directory.

--- embed/example2.c
+++ embed/example2.c
@@ -4,9 +4,11 @@
 
 int main(int argc, char** argv)
 {
+  
   yacas_init();
   yacas_eval("D(x)Sin(x)");
   if (!yacas_error()) printf("%s\n",yacas_result());  
+  else printf("%s\n", yacas_error());
   yacas_exit();
   return 0;
 }
