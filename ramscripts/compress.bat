36cdc81f2cac84640d357a5ac8a5c9bfe846f44d
Ayal Pinkus <apinkus@xs4all.nl>
2003-01-27 19:03:22 +0000

    Improved windows binary build sequence: makewin.bat now builds the entire binary release into winrel/.

--- ramscripts/compress.bat
+++ ramscripts/compress.bat
@@ -6,11 +6,11 @@ call cl win32rcwriter.cpp
 win32rcwriter.exe
 copy scripts.rc ..\src\
 cd ..\src
-pause
+rem pause
 call msdev mkfastprimes.dsp /MAKE "mkfastprimes - Win32 Release" 
 release\mkfastprimes.exe > fastprimes.c
 call msdev yacasdll.dsp /MAKE "yacasdll - Win32 Release" 
 call msdev yacas.dsp /MAKE "yacas - Win32 Release" 
-pause
+rem pause
 
 
