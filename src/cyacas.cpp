0a16ccab1f876d0b1f5796871cb3c35ce5e51c48
Ayal Pinkus <apinkus@xs4all.nl>
2005-07-17 19:55:12 +0000

    Extend the API provided to also allow creating lower-level objects and evaluate them, in stead of just passing a string. example4 in embed/ demomstrates this.

--- src/cyacas.cpp
+++ src/cyacas.cpp
@@ -10,6 +10,7 @@
 #include "stringio.h"
 #include "yacas.h"
 #include "archiver.h"
+#include "standard.h"
 
 static CYacas *yacas = NULL;
 LispString *stringout = NULL;
@@ -119,4 +120,126 @@ void yacas_exit()
     yacas = NULL;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+// Creating objects
+void* yacas_create_atom(char* atom)
+{
+  return LispAtom::New((*yacas)()(),atom);
+}
+void* yacas_create_string(char* string)
+{
+  LispString orig(string);
+  LispString stringified;
+  InternalStringify(stringified, &orig);
+  return LispAtom::New((*yacas)()(),stringified.String());
+}
+void* yacas_create_number_from_string(char* string)
+{
+  return LispAtom::New((*yacas)()(),string);
+}
+void* yacas_create_number_from_long(long number)
+{
+  BigNumber *z = new BigNumber();
+  z->SetTo(number);
+  return NEW LispNumber(z);
+}
+void* yacas_create_number_from_double(double number)
+{
+  BigNumber *z = new BigNumber();
+  z->SetTo(number);
+  return NEW LispNumber(z);
+}
+void* yacas_create_sublist(void* object)
+{
+  return LispSubList::New((LispObject*)object);
+}
+
+// Linking linked lists
+void* yacas_link_objects(void* head, void* tail)
+{
+  LispObject *h = (LispObject *)head;
+  LispObject *t = (LispObject *)tail;
+  h->Next().Set(t);
+  return head;
+}
+
+// executing an object
+void* yacas_execute(void* object)
+{
+  LispPtr result;
+  LispPtr input;
+  LispObject* oin = ((LispObject*)object);
+  input.Set(oin);
+  (*yacas)()().iEvaluator->Eval((*yacas)()(), result, input);
+
+  oin->IncreaseRefCount();
+  input.Set(NULL);
+  oin->DecreaseRefCount();
+  LispObject* oout = result.Get();
+  oout->IncreaseRefCount();
+  result.Set(NULL);
+  oout->DecreaseRefCount();
+  return oout;
+}
+
+// pulling apart an object again
+void* yacas_get_sublist(void* object)
+{
+  return ((LispObject*)object)->SubList()->Get();
+}
+char* yacas_get_atom(void* object)
+{
+  return ((LispObject*)object)->String()->String();
+}
+int  yacas_object_is_string(void* object)
+{
+  if (((LispObject*)object)->String() == NULL)
+    return 0;
+  if (((LispObject*)object)->String()->String()[0] != '\"')
+    return 0;
+  return 1;
+}
+int  yacas_object_is_sublist(void* object)
+{
+  return (((LispObject*)object)->SubList() != NULL);
+}
+int  yacas_object_is_atom(void* object)
+{
+  return (((LispObject*)object)->String() != NULL);
+}
+int  yacas_object_is_number(void* object)
+{
+  return (((LispObject*)object)->Number(0) != NULL);
+}
+int  yacas_object_is_integer(void* object)
+{
+  return (((LispObject*)object)->Number(0)->IsInt());
+}
+long yacas_get_long(void* object)
+{
+  return (long)(((LispObject*)object)->Number(0)->Double());
+}
+double yacas_get_double(void* object)
+{
+  return (((LispObject*)object)->Number(0)->Double());
+}
+
+void* yacas_get_next(void* object)
+{
+  return (((LispObject*)object)->Next().Get());
+}
+void yacas_delete_object(void* object)
+{
+  delete ((LispObject*)object);
+}
 
