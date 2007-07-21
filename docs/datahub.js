cf1db6965d15442315b2972af7b55031f11d368a
Ayal Pinkus <apinkus@xs4all.nl>
2007-07-21 22:58:01 +0000

    Small fixups to the initial journal entry (links to trying it out and editing accompanying code almost working again).

--- docs/datahub.js
+++ docs/datahub.js
@@ -4,6 +4,12 @@ function addDatahub()
   document.write(' <APPLET name="datahub" archive="yacas.jar" code="net.sf.yacas.DatahubApplet" width=1 height=1></APPLET>');
 }
 
+function addDatahubInJournal()
+{
+  document.write(' <APPLET name="datahub" archive="../yacas.jar" code="net.sf.yacas.DatahubApplet" width=1 height=1></APPLET>');
+}
+
+
 function getDatahub()
 {
   var datahub;
