a274483d46682ff7697a7f8e34d635d616aa8c6a
ayalpinkus <ayalpinkus@ba8beb34-e082-4677-a38a-25dc9495808b>
2007-07-25 20:59:49 +0000

    Many little web site improvements.

--- trunk/docs/datahub.js
+++ trunk/docs/datahub.js
@@ -4,11 +4,6 @@ function addDatahub()
   document.write(' <APPLET name="datahub" archive="yacas.jar" code="net.sf.yacas.DatahubApplet" width=1 height=1></APPLET>');
 }
 
-function addDatahubInJournal()
-{
-  document.write(' <APPLET name="datahub" archive="../yacas.jar" code="net.sf.yacas.DatahubApplet" width=1 height=1></APPLET>');
-}
-
 
 function getDatahub()
 {
