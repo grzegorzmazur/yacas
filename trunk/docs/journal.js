ffeaede3363959b1cc97a80d090e376d38cb9592
ayalpinkus <ayalpinkus@ba8beb34-e082-4677-a38a-25dc9495808b>
2007-08-01 16:57:38 +0000

    Work in progress.

--- trunk/docs/journal.js
+++ trunk/docs/journal.js
@@ -3,6 +3,6 @@
 /* When deep link found, redirect to the tabbified main page */
 if (top.location == self.location)
 {
-  self.location.replace("../homepage.html?"+self.location.href);
+  self.location.replace("yacas.html");
 }
 
