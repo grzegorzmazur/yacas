3d70c954eb5a30957bccb5eff31013de1ddfef50
Ayal Pinkus <apinkus@xs4all.nl>
2007-08-17 19:52:44 +0000

    Trying to make the web site load faster.

--- docs/datahub.js
+++ docs/datahub.js
@@ -1,9 +1,4 @@
 
-function addDatahub()
-{
-  document.write(' <APPLET name="datahub" archive="yacas.jar" code="net.sf.yacas.DatahubApplet" width=1 height=1></APPLET>');
-}
-
 
 function getDatahub()
 {
@@ -16,3 +11,6 @@ function alertUpgradeJava()
   alert("Something seems to be wrong with Java support, the Yacas calculation center does not seem to be available. "+
         "You might need to download a newer version of Java for your system from http://www.java.com/ (Java 1.5 is required).");
 }
+
+
+
