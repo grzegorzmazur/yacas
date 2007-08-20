9754cb7e15e50b300c4177f819f5fb917d6e2276
Ayal Pinkus <apinkus@xs4all.nl>
2007-08-20 10:24:19 +0000

    Examples on the web site.

--- docs/autocompleter.cpp
+++ docs/autocompleter.cpp
@@ -24,6 +24,51 @@ char* escape(char* text)
 }
 FILE*fout;
 int first = 1;
+
+struct Example
+{
+  char name[32];
+  char example[224];
+};
+
+Example examples[2048];
+int nrExamples = 0;
+
+int exampleCompare(const void *v1, const void *v2)
+{
+  Example* e1 = (Example*)v1;
+  Example* e2 = (Example*)v2;
+  return strcmp(e1->name,e2->name);
+}
+
+char *items[10];
+int nrItems=0;
+void getfields(char* buffer)
+{
+  nrItems=0;
+  char* ptr = buffer;
+  items[nrItems++] = ptr;
+  while (*ptr)
+  {
+    while (*ptr && *ptr != ':') ptr++;
+    if (*ptr)
+    {
+      *ptr++ = '\0';
+      items[nrItems++] = ptr;    
+    }
+  }
+}
+
+char* findexample(char* name)
+{
+  Example key;
+  strcpy(key.name,name);
+  void *found = bsearch(&key, examples, nrExamples, sizeof(Example),exampleCompare);
+  if (found)
+    return ((Example*) found)->example;
+  return "";
+}
+
 void WriteLine()
 {
   if (lastKey[0])
@@ -35,15 +80,18 @@ void WriteLine()
     first = 0;
     fprintf(fout,"\'%s\',\n",lastKey);
     fprintf(fout,"\'%s",escape(lastVal));
-    fprintf(fout,"%s\'\n",escape(lastDes));
+    fprintf(fout,"%s\',\n",escape(lastDes));
+    fprintf(fout,"\'%s\'\n",findexample(lastKey)); // placeholder for examples
     lastKey[0] = '\0';
     lastVal[0] = '\0';
     lastDes[0] = '\0';
   }
 }
 
+
 int main(int argc, char** argv)
 {
+  char buffer[16384];
   lastKey[0] = '\0';
   lastVal[0] = '\0';
   lastDes[0] = '\0';
@@ -54,6 +102,26 @@ int main(int argc, char** argv)
     inName = argv[1];
     outName = argv[2];
   }
+  {
+    FILE* fex = fopen("examples-static.txt","rb");
+    if (!fex)
+      exit(-1);
+    fgets(buffer,16384,fex);
+    while (!feof(fex))
+    {
+      getfields(buffer);
+      if (nrItems>1)
+      {
+        strcpy(examples[nrExamples].name,items[0]);
+        strcpy(examples[nrExamples].example,items[1]);
+        nrExamples++;
+      }
+      fgets(buffer,16384,fex);
+    } 
+    fclose(fex);
+    qsort(examples, nrExamples, sizeof(Example),exampleCompare);
+  }
+
   FILE*fin=fopen(inName,"rb");
   if (!fin)exit(-1);
   fout=fopen(outName,"wb");
@@ -61,23 +129,10 @@ int main(int argc, char** argv)
 
   fprintf(fout,"var hints = new Array(\n");
 
-  char buffer[16384];
   fgets(buffer,16384,fin);
   while (!feof(fin))
   {
-    char *items[10];
-    int nrItems=0;
-    char* ptr = buffer;
-    items[nrItems++] = ptr;
-    while (*ptr)
-    {
-      while (*ptr && *ptr != ':') ptr++;
-      if (*ptr)
-      {
-        *ptr++ = '\0';
-        items[nrItems++] = ptr;    
-      }
-    }
+    getfields(buffer);
     if (nrItems>3)
     {
       if (strcmp(lastKey,items[1]))
