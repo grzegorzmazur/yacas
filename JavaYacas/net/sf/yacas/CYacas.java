844aa864a7e24f2f11896356537ff9dad99dcac8
Ayal Pinkus <apinkus@xs4all.nl>
2007-09-05 18:33:21 +0000

    Finishing up the stop button.

--- JavaYacas/net/sf/yacas/CYacas.java
+++ JavaYacas/net/sf/yacas/CYacas.java
@@ -24,6 +24,10 @@ public class CYacas
     String rs = "";
     try
     {
+       env.iEvalDepth=0;
+       env.iEvaluator.ResetStack();
+
+
       iError = null;
 
       LispPtr in_expr = new LispPtr();
