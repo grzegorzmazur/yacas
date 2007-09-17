9a6a1c2ec3e32ad25f989fdb4de90b91e9e00b10
Ayal Pinkus <apinkus@xs4all.nl>
2007-09-17 12:49:02 +0000

    post-release update.

--- JavaYacas/net/sf/yacas/CVersion.java
+++ JavaYacas/net/sf/yacas/CVersion.java
@@ -1,2 +1,2 @@
 package net.sf.yacas;
-class CVersion { static String VERSION = "1.1.18"; }
+class CVersion { static String VERSION = "1.1.19"; }
--- src/version.h
+++ src/version.h
@@ -1,1 +1,1 @@
-#define VERSION "1.1.18"
+#define VERSION "1.1.19"
