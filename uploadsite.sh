e8dfc9d47b9b34ed9ccdc1f6cbf745ca099896d9
Grzegorz Mazur <teoretyk@gmail.com>
2013-02-28 16:06:18 +0000

    minor janitorial work

--- uploadsite.sh
+++ uploadsite.sh
@@ -7,6 +7,6 @@ if [ -z "$SFUSER" ]; then
     exit 1
 fi
 
-ssh $SFUSER,yacas@shell.sourceforge.net create
-cat homepage.tgz | ssh $SFUSER@shell.sourceforge.net 'tar zxvf - -C /home/project-web/yacas/htdocs'
-ssh $SFUSER,yacas@shell.sourceforge.net shutdown
+ssh -x $SFUSER,yacas@shell.sourceforge.net create
+cat homepage.tgz | ssh -x $SFUSER@shell.sourceforge.net 'tar zxvf - -C /home/project-web/yacas/htdocs'
+ssh -x $SFUSER,yacas@shell.sourceforge.net shutdown
