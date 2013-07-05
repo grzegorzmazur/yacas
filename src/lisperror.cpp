c04782ceac742a0bf61212b54abfbcb8b6531f96
Grzegorz Mazur <teoretyk@gmail.com>
2013-07-05 18:05:50 +0000

    make the codereviewer happy about whitespaces

--- src/lisperror.cpp
+++ src/lisperror.cpp
@@ -5,16 +5,16 @@
 void Handle(LispInt aError, LispEnvironment& aEnvironment, LispOutput& aOutput)
 {
     if (aEnvironment.ErrorString(aError)[0] != '\0') {
-        if (aEnvironment.iInputStatus.LineNumber() >= 0) { 
-            LispChar linenum[20]; 
-            InternalIntToAscii(linenum,aEnvironment.iInputStatus.LineNumber()); 
+        if (aEnvironment.iInputStatus.LineNumber() >= 0) {
+            LispChar linenum[20];
+            InternalIntToAscii(linenum,aEnvironment.iInputStatus.LineNumber());
             aOutput.Write(aEnvironment.iInputStatus.FileName());
             aOutput.Write("(");
             aOutput.Write(linenum);
             aOutput.Write(") : ");
-        } 
-        aEnvironment.iCleanup.Delete(); 
-        aOutput.Write(aEnvironment.ErrorString(aError)); 
-        aOutput.Write("\n"); 
+        }
+        aEnvironment.iCleanup.Delete();
+        aOutput.Write(aEnvironment.ErrorString(aError));
+        aOutput.Write("\n");
     }
 }
