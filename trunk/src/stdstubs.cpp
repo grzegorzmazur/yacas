be94dbff8019e18e60e3368f28b06adfea12b73c
grzesiek <grzesiek@ba8beb34-e082-4677-a38a-25dc9495808b>
2013-07-14 11:45:18 +0000

    make the -DNO_GLOBALS mode compile and work

--- trunk/src/stdstubs.cpp
+++ trunk/src/stdstubs.cpp
@@ -5,24 +5,8 @@
 #include "lisperror.h"
 #include "stubs.h"
 
-/* If we have no globals, don't compile in obmalloc, but use the
- standard allocators */
-#ifdef NO_GLOBALS
-#define PlatStubAlloc PlatAlloc
-#define PlatStubReAlloc PlatReAlloc
-#define PlatStubFree PlatFree
-#endif
-
 void * PlatStubAlloc(LispInt aNrBytes)
 {
-    /*Test code: report on memory usage
-     static int nrsmall=0,nrlarge=0;
-    if (aNrBytes < 256)
-        nrsmall++;
-    else
-        nrlarge++;
-    printf("%d, %d\n",nrsmall,nrlarge);
-    */
     void * result = malloc(aNrBytes);
     Check(result!=NULL,KLispErrNotEnoughMemory);
     return result;
