4af035d347149d120054717930f6214ecc90fcfe
Grzegorz Mazur <teoretyk@gmail.com>
2013-12-21 13:04:10 +0000

    minor improvements in parser error handling

--- src/errors.h
+++ src/errors.h
@@ -1,7 +1,13 @@
 #ifndef YACAS_ERRORS_H
 #define YACAS_ERRORS_H
 
-void CheckArgType(LispInt aArgNr, LispPtr& aArguments, LispEnvironment& aEnvironment, LispInt aError);
+#ifdef __GNUC__
+#  define YACAS_NORETURN __attribute__((noreturn))
+#else
+#  define YACAS_NORETURN
+#endif
+
+void CheckArgType(LispInt aArgNr, LispPtr& aArguments, LispEnvironment& aEnvironment, LispInt aError) YACAS_NORETURN;
 #define CHK_ARG(_pred,_argnr)           {if (!(_pred))                              CheckArgType(_argnr,aArguments, aEnvironment,KLispErrInvalidArg);}
 #define CHK_ARG_CORE(_pred,_argnr)      {if (!(_pred))                              CheckArgType(_argnr,ARGUMENT(0),aEnvironment,KLispErrInvalidArg);}
 #define CHK_ISLIST(_pred,_argnr)        {if (!InternalIsList(_pred))                     CheckArgType(_argnr,aArguments, aEnvironment,KLispErrNotList);}
@@ -10,23 +16,15 @@ void CheckArgType(LispInt aArgNr, LispPtr& aArguments, LispEnvironment& aEnviron
 #define CHK_ISSTRING_CORE(_pred,_argnr) {if (!InternalIsString((_pred)->String())) CheckArgType(_argnr,ARGUMENT(0),aEnvironment,KLispErrNotString);}
 
 void CheckNrArgs(LispInt n, LispPtr& aArguments, LispEnvironment& aEnvironment);
-#define TESTARGS(_n)  CheckNrArgs(_n,aArguments,aEnvironment)
 
-void CheckFuncGeneric(LispInt aError,LispPtr& aArguments,LispEnvironment& aEnvironment);
-void CheckFuncGeneric(LispInt aError,LispEnvironment& aEnvironment);
+void CheckFuncGeneric(LispInt aError,LispPtr& aArguments,LispEnvironment& aEnvironment) YACAS_NORETURN;
+void CheckFuncGeneric(LispInt aError,LispEnvironment& aEnvironment) YACAS_NORETURN;
+
 #define CHK(_pred,_err)      {if (!(_pred)) CheckFuncGeneric(_err,aArguments, aEnvironment);}
 #define CHK_CORE(_pred,_err) {if (!(_pred)) CheckFuncGeneric(_err,ARGUMENT(0),aEnvironment);}
 #define CHK2(_pred,_err)     {if (!(_pred)) CheckFuncGeneric(_err,aEnvironment);}
 
 char *GenericErrorBuf(); // called (only) from errors.cpp and LispEnvironment::ErrorString()
 void RaiseError(const char* str,...);
-
-/*
-#define TESTARGS(_n)  \
-    { \
-      LispInt nrArguments = InternalListLength(aArguments);  \
-      Check(nrArguments == _n,KLispErrWrongNumberOfArgs); \
-    }
-*/
 
 #endif
