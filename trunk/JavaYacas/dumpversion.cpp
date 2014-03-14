562b3184c8b4bac8766fee51b9c4cb80f89356f0
grzesiek <grzesiek@ba8beb34-e082-4677-a38a-25dc9495808b>
2014-03-14 09:11:41 +0000

    make java engine use the new directory layout

--- trunk/JavaYacas/dumpversion.cpp
+++ trunk/JavaYacas/dumpversion.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "version.h"
+#include "yacas/yacas_version.h"
 
 int main(int argc, char** argv)
 {
   printf("package net.sf.yacas;\n");
-  printf("class CVersion { static String VERSION = \"%s\"; }\n", VERSION);
+  printf("class CVersion { static String VERSION = \"%s\"; }\n", YACAS_VERSION);
   return 0;
 }
 
