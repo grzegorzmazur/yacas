9dbea1108545b764ddb1c937bdaf19ebb952b868
grzesiek <grzesiek@ba8beb34-e082-4677-a38a-25dc9495808b>
2014-04-19 22:11:46 +0000

    some const-correctness fixes

--- trunk/include/yacas/standard.h
+++ trunk/include/yacas/standard.h
@@ -13,11 +13,11 @@ class LispHashTable;
 
 bool InternalIsList(const LispPtr& aPtr);
 bool InternalIsString(LispString * aOriginal);
-void InternalUnstringify(LispString& aResult, LispString * aOriginal);
-void InternalStringify(LispString& aResult, LispString * aOriginal);
+void InternalUnstringify(LispString& aResult, const LispString* aOriginal);
+void InternalStringify(LispString& aResult, const LispString* aOriginal);
 void InternalIntToAscii(LispChar * aTrg,LispInt aInt);
-LispInt InternalAsciiToInt(LispString * aString);
-bool IsNumber(const LispChar * ptr,bool aAllowFloat);
+LispInt InternalAsciiToInt(const LispString* aString);
+bool IsNumber(const LispChar * ptr, bool aAllowFloat);
 
 void InternalNth(LispPtr& aResult, const LispPtr& aArg, LispInt n);
 void InternalTail(LispPtr& aResult, const LispPtr& aArg);
@@ -25,33 +25,33 @@ void InternalAssociate(LispPtr& aResult, const LispPtr& aKey,
                       const LispPtr& aAssociationList);
 
 void InternalReverseList(LispPtr& aResult, const LispPtr& aOriginal);
-void InternalFlatCopy(LispPtr& aResult, LispPtr& aOriginal);
-LispInt InternalListLength(LispPtr& aOriginal);
+void InternalFlatCopy(LispPtr& aResult, const LispPtr& aOriginal);
+LispInt InternalListLength(const LispPtr& aOriginal);
 
 bool InternalEquals(LispEnvironment& aEnvironment,
-                           LispPtr& aExpression1,
-                           LispPtr& aExpression2);
+                    const LispPtr& aExpression1,
+                    const LispPtr& aExpression2);
 
 
 inline LispPtr& Argument(LispPtr& cur, LispInt n);
 
-inline void InternalTrue(LispEnvironment& aEnvironment, LispPtr& aResult);
-inline void InternalFalse(LispEnvironment& aEnvironment, LispPtr& aResult);
+inline void InternalTrue(const LispEnvironment& aEnvironment, LispPtr& aResult);
+inline void InternalFalse(const LispEnvironment& aEnvironment, LispPtr& aResult);
 inline void InternalBoolean(LispEnvironment& aEnvironment, LispPtr& aResult,
                             bool aValue);
-inline bool IsTrue(LispEnvironment& aEnvironment, LispPtr& aExpression);
-inline bool IsFalse(LispEnvironment& aEnvironment, LispPtr& aExpression);
+inline bool IsTrue(LispEnvironment& aEnvironment, const LispPtr& aExpression);
+inline bool IsFalse(LispEnvironment& aEnvironment, const LispPtr& aExpression);
 inline void InternalNot(LispPtr& aResult, LispEnvironment& aEnvironment, LispPtr& aExpression);
 
 void DoInternalLoad(LispEnvironment& aEnvironment,LispInput* aInput);
-void InternalLoad(LispEnvironment& aEnvironment,LispString * aFileName);
+void InternalLoad(LispEnvironment& aEnvironment,const LispString* aFileName);
 void InternalUse(LispEnvironment& aEnvironment,LispString * aFileName);
 void InternalApplyString(LispEnvironment& aEnvironment, LispPtr& aResult,
                          LispString * aOperator,LispPtr& aArgs);
 void InternalApplyPure(LispPtr& oper,LispPtr& args2,LispPtr& aResult,LispEnvironment& aEnvironment);
 
 void InternalEvalString(LispEnvironment& aEnvironment, LispPtr& aResult,
-                        LispChar * aString);
+                        const LispChar* aString);
 
 #define ATOML(_s) LispAtom::New(aEnvironment,_s)
 #define LIST(_c) LispSubList::New(_c)
@@ -75,7 +75,8 @@ void ReturnUnEvaluated(LispPtr& aResult,LispPtr& aArguments,
  limiting it to a maximum number of characters. If aMaxChars
  is less than zero, the result is not truncated.
  */
-void PrintExpression(LispString& aResult, LispPtr& aExpression,
+void PrintExpression(LispString& aResult,
+                     LispPtr& aExpression,
                      LispEnvironment& aEnvironment,
                      LispInt aMaxChars);
 
