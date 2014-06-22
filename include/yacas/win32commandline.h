1d408bc82a2f4a6794b49e6120dc56e871138781
Grzegorz Mazur <teoretyk@gmail.com>
2014-06-22 19:56:03 +0000

    win32 console rewrite

--- include/yacas/win32commandline.h
+++ include/yacas/win32commandline.h
@@ -1,8 +1,6 @@
 #ifndef YACAS_WIN32COMMANDLINE_H
 #define YACAS_WIN32COMMANDLINE_H
 
-#include <memory>
-
 #include <windows.h>
 
 #include "commandline.h"
@@ -12,24 +10,21 @@ class CWin32CommandLine : public CCommandLine
 public:
     CWin32CommandLine();
     ~CWin32CommandLine();
-public:
-    virtual LispInt GetKey();
-    void ReadLineSub(const std::string& prompt);
-    virtual void NewLine();
-    virtual void ShowLine(const std::string& prompt, unsigned cursor);
-    virtual void Pause();
 
-    // new functionality
-    void color_print(const std::string& str, WORD text_attrib);
-    void color_read(LispChar * str, WORD text_attrib);
-protected:
-    void ShowLine();
-    std::string last_prompt;
+    LispInt GetKey();
+    void NewLine();
+    void ShowLine(const std::string& prompt, unsigned cursor);
+    void Pause();
+    void MaxHistoryLinesSaved(std::size_t);
+
 private:
+    void color_print(const std::string& str, WORD text_attrib);
+
     HANDLE out_console;
-    HANDLE in_console;
-    static const std::size_t BUF_SIZE = 16384;
-    std::unique_ptr<char []> _buf;
+
+    SHORT _cursor_line, _last_line;
+
+    std::size_t _max_lines;
 };
 
 #endif
