7c9d2b8aca65ee6d9c55a4f4840230c05945a765
grzesiek <grzesiek@ba8beb34-e082-4677-a38a-25dc9495808b>
2014-08-31 11:03:12 +0000

    cleanup

--- trunk/include/yacas/arrayclass.h
+++ trunk/include/yacas/arrayclass.h
@@ -9,19 +9,51 @@
 
 class ArrayClass : public GenericClass
 {
-public: //required
-    ArrayClass(LispInt aSize,LispObject* aInitialItem);
-    virtual ~ArrayClass() = default;
-    virtual const LispChar * TypeName();
-public: //array-specific
-    inline LispInt Size();
-    inline LispObject* GetElement(LispInt aItem); // TODO: 1-based, ...
-    inline void SetElement(LispInt aItem,LispObject* aObject);
+public:
+    //required
+    ArrayClass(std::size_t aSize,LispObject* aInitialItem);
+    const LispChar* TypeName();
+
+    //array-specific
+    std::size_t Size();
+    LispObject* GetElement(std::size_t aItem);
+    void SetElement(std::size_t aItem,LispObject* aObject);
 
 private:
     std::vector<LispPtr> iArray;
 };
 
-#include "arrayclass.inl"
+inline
+ArrayClass::ArrayClass(std::size_t aSize, LispObject* aInitialItem):
+    iArray(aSize, LispPtr(aInitialItem))
+{
+}
+
+inline
+const LispChar* ArrayClass::TypeName()
+{
+    return "\"Array\"";
+}
+
+inline
+std::size_t ArrayClass::Size()
+{
+    return iArray.size();
+}
+
+inline
+LispObject* ArrayClass::GetElement(std::size_t aItem)
+{
+    assert(aItem > 0 && aItem<=iArray.size());
+    return iArray[aItem-1];
+}
+
+inline
+void ArrayClass::SetElement(std::size_t aItem, LispObject* aObject)
+{
+    assert(aItem > 0 && aItem<=iArray.size());
+    iArray[aItem-1] = aObject;
+}
+
 #endif
 
