b2c49a64b0e406de6da3d47eeacf74f68065a83b
Grzegorz Mazur <teoretyk@gmail.com>
2014-09-29 10:19:57 +0000

    minor cleanup of the ANumber class interface

--- include/yacas/anumber.h
+++ include/yacas/anumber.h
@@ -32,11 +32,20 @@ public:
     void CopyFrom(const ANumber& aOther);
     bool ExactlyEqual(const ANumber& aOther);
     void SetTo(const LispChar * aString,LispInt aBase=10);
-    inline void SetPrecision(LispInt aPrecision) {iPrecision = aPrecision;}
+    LispInt Precision() const;
+    void SetPrecision(LispInt aPrecision) {iPrecision = aPrecision;}
     void ChangePrecision(LispInt aPrecision);
     void RoundBits(void);
     void DropTrailZeroes();
     void Expand();
+
+    void Negate();
+
+    bool IsZero() const;
+    bool IsNegative() const;
+    bool IsEven() const;
+
+    void Print(const std::string& prefix) const;
 
 public:
     LispInt iExp;
@@ -45,11 +54,11 @@ public:
     LispInt iTensExp;
 };
 
-inline bool IsPositive(ANumber& a) { return !a.iNegative; }
-inline bool IsNegative(ANumber& a) { return a.iNegative;  }
-inline bool IsEven(ANumber& a) { return ((a[0]&1) == 0); }
-inline bool IsOdd(ANumber& a)  { return ((a[0]&1) == 1); }
-inline LispInt     Precision(ANumber& a) { return a.iPrecision; }
+inline
+LispInt ANumber::Precision() const
+{
+    return iPrecision;
+}
 
 bool BaseLessThan(ANumber& a1, ANumber& a2);
 void BaseDivide(ANumber& aQuotient, ANumber& aRemainder, ANumber& a1, ANumber& a2);
@@ -62,7 +71,7 @@ LispInt WordDigits(LispInt aPrecision, LispInt aBase);
 
 // Operations on ANumber.
 void Negate(ANumber& aNumber);
-void  ANumberToString(LispString& aResult, ANumber& aNumber, LispInt aBase, bool aForceFloat=0);
+void  ANumberToString(LispString& aResult, ANumber& aNumber, LispInt aBase, bool aForceFloat=false);
 void Add(ANumber& aResult, ANumber& a1, ANumber& a2);
 void Subtract(ANumber& aResult, ANumber& a1, ANumber& a2);
 void Multiply(ANumber& aResult, ANumber& a1, ANumber& a2);
@@ -74,9 +83,40 @@ void BaseShiftLeft(ANumber& a, LispInt aNrBits);
 void BaseGcd(ANumber& aResult, ANumber& a1, ANumber& a2);
 void Sqrt(ANumber& aResult, ANumber& N);
 
-void PrintNumber(const char* prefix,ANumber& aNumber);
-
 void NormalizeFloat(ANumber& a2, LispInt digitsNeeded);
+
+inline
+void ANumber::Negate()
+{
+    iNegative = !iNegative;
+
+    // FIXME: do we need negative zero?
+    if (IsZero())
+        iNegative = false;
+}
+
+
+inline
+bool ANumber::IsZero() const
+{
+    for (const PlatWord& a: *this)
+        if (a != 0)
+            return false;
+
+    return true;
+}
+
+inline
+bool ANumber::IsNegative() const
+{
+    return iNegative;
+}
+
+inline
+bool ANumber::IsEven() const
+{
+    return (front() & 1) == 0;
+}
 
 #include "anumber.inl"
 
