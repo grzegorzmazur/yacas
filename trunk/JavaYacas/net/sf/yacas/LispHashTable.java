f420f7ee85d57188bb514586a70c317f5034f228
grzesiek <grzesiek@ba8beb34-e082-4677-a38a-25dc9495808b>
2015-02-12 13:19:35 +0000

    Re-implement LispHashTable in terms of  String.intern(); related #64

--- trunk/JavaYacas/net/sf/yacas/LispHashTable.java
+++ trunk/JavaYacas/net/sf/yacas/LispHashTable.java
@@ -1,33 +1,16 @@
 package net.sf.yacas;
 
-class LispHashTable
-{
-    // If string not yet in table, insert. Afterwards return the string.
-    String LookUp(String aString)
-  {
-    if (!iHashtable.containsKey(aString))
-      iHashtable.put(aString,aString);
-    return (String)iHashtable.get(aString);
-  }
-    String LookUpStringify(String aString)
-  {
-    aString = "\""+aString+"\"";
-    if (!iHashtable.containsKey(aString))
-      iHashtable.put(aString,aString);
-    return (String)iHashtable.get(aString);
-  }
-    String LookUpUnStringify(String aString)
-  {
-    aString = aString.substring(1,aString.length()-1);
-    if (!iHashtable.containsKey(aString))
-      iHashtable.put(aString,aString);
-    return (String)iHashtable.get(aString);
-  }
+class LispHashTable {
 
-    // GarbageCollect
-    void GarbageCollect()
-  {
-    //TODO FIXME
-  }
-  java.util.Hashtable iHashtable = new java.util.Hashtable();
+    String LookUp(String aString) {
+        return aString.intern();
+    }
+
+    String LookUpStringify(String aString) {
+        return ("\"" + aString + "\"").intern();
+    }
+
+    String LookUpUnStringify(String aString) {
+        return aString.substring(1, aString.length() - 1).intern();
+    }
 }
