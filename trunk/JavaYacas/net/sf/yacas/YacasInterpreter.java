ad21f38c70bde599e057066bd2fc592b55c57260
grzesiek <grzesiek@ba8beb34-e082-4677-a38a-25dc9495808b>
2015-02-21 07:56:37 +0000

    wrap yacas tests in junit to facilitate testing the java engine; fixes #74

--- trunk/JavaYacas/net/sf/yacas/YacasInterpreter.java
+++ trunk/JavaYacas/net/sf/yacas/YacasInterpreter.java
@@ -1,5 +1,9 @@
 package net.sf.yacas;
 
+import java.io.*;
+import java.net.*;
+import java.nio.file.NoSuchFileException;
+import java.util.zip.*;
 
 /**
  * Use this class in order to access the Yacas interpreter from an external application.
@@ -13,45 +17,40 @@ package net.sf.yacas;
  * @author av
  */
 public class YacasInterpreter {
- 
+
     private CYacas yacas;
- 
+
     /** Creates a new instance of YacasInterpreter */
-    public YacasInterpreter() {
-        yacas = new CYacas(new StringOutput(new StringBuffer()));
-        boolean scripts_found = loadScripts();
- 
-        if (!scripts_found) System.err.println("Yacas error: Unable to load yacas.jar");
-        yacas.Evaluate("Load(\"yacasinit.ys\");");
- 
+    public YacasInterpreter()  throws IOException, ZipException, URISyntaxException {
+        this(new StringOutput(new StringBuffer()));
     }
- 
-    /** Searches for the file yacas.jar and passes its absolute path to the Yacas interpreter.
-     * This method searches in the classpath (declared i.e. in MANIFEST.MF) for the file yacasinit.ys.
-     * yacasinit.ys is inside yacas.jar.
-     * Returns true if successful.*/
-    private boolean loadScripts() {
-        java.net.URL detectURL = java.lang.ClassLoader.getSystemResource("scripts/yacasinit.ys");
-        // if yacasinit.ys not found:
-        if (detectURL == null) return false;
- 
-        String detect = detectURL.getPath(); // file:/home/av/src/lib/yacas.yar!/yacasinit.ys
-        String archive = detect.substring(0, detect.lastIndexOf('!')); // file:/home/av/src/lib/yacas.jar
- 
-        java.util.zip.ZipFile z;
-        try {
-            z = new java.util.zip.ZipFile(new java.io.File(new java.net.URI(archive)));
-        } catch (Exception e) {
-            System.err.println(e.toString());
-            return false;
+
+    public YacasInterpreter(LispOutput out) throws IOException, ZipException, URISyntaxException {
+
+        String scriptsDir = "scripts/";
+
+        yacas = new CYacas(out);
+
+        URL initURL = java.lang.ClassLoader.getSystemResource(scriptsDir + "yacasinit.ys");
+
+        if (initURL == null)
+            throw new NoSuchFileException("yacasinit.ys not found in " + scriptsDir);
+
+        String initPath = initURL.getPath();
+
+        if (initPath.lastIndexOf('!') >= 0) {
+            String archive = initPath.substring(0, initPath.lastIndexOf('!'));
+
+            ZipFile z = new ZipFile(new File(new URI(archive)));
+
+            LispStandard.zipFile = z;
+        } else {
+            yacas.Evaluate("DefaultDirectory(\"" + scriptsDir + "\");");
         }
- 
-        // Pass the absolute path of yacas.jar to Yacas.
-        LispStandard.zipFile = z;
- 
-        return true;
+
+        yacas.Evaluate("Load(\"yacasinit.ys\");");
     }
- 
+
     /** Use this method to pass an expression to the Yacas interpreter.
      *  Returns the output of the interpreter.
      */
