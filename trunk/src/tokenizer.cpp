b67dd1ec31af649048bb532358e276b3c215862f
grzesiek <grzesiek@ba8beb34-e082-4677-a38a-25dc9495808b>
2015-04-06 09:01:59 +0000

    do not use StartPtr in LispTokenizer, related #75

--- trunk/src/tokenizer.cpp
+++ trunk/src/tokenizer.cpp
@@ -1,23 +1,10 @@
-
-#include "yacas/yacasprivate.h"
 #include "yacas/tokenizer.h"
 
-/* Default parse algorithm:
- *
- * Numbers: start with + or -, and continues to have digits or .
- * ( and ) as brackets
- * Alpha atom: First char isalpha, and after that isalnum.
- * Oper atom:  all chars have to be symbolic
- *
- */
-
-#include "yacas/yacasprivate.h"
 #include "yacas/lisperror.h"
-#include "yacas/numbers.h"
 
-#include <cassert>
-
-static const char symbolics[] = "~`!@#$^&*-=+:<>?/\\|";
+namespace {
+    static const char symbolics[] = "~`!@#$^&*-=+:<>?/\\|";
+}
 
 bool IsSymbolic(LispChar c)
 {
@@ -31,126 +18,140 @@ bool IsSymbolic(LispChar c)
 const LispString* LispTokenizer::NextToken(LispInput& aInput,
                                            LispHashTable& aHashTable)
 {
-  LispChar c;
-  LispInt firstpos;
+    char c;
 
-REDO:
-  firstpos = aInput.Position();
-
-  // End of stream: return empty string
-  if (aInput.EndOfStream())
-    goto FINISH;
-
-  c = aInput.Next();
-  //printf("%c",c);
-
-  //Parse brackets
-  if (c == '(')      {}
-  else if (c == ')') {}
-  else if (c == '{') {}
-  else if (c == '}') {}
-  else if (c == '[') {}
-  else if (c == ']') {}
-  else if (c == ',') {}
-  else if (c == ';') {}
-  else if (c == '%') {}
-  else if (c == '.' && !std::isdigit(aInput.Peek()) )
-  {
-    while (aInput.Peek() == '.')
-    {
-      aInput.Next();
-    }
-  }
-  // parse comments
-  else if (c == '/' && aInput.Peek() == '*')
-  {
-    aInput.Next(); //consume *
-FALSEALARM:
-    while (aInput.Next() != '*' && !aInput.EndOfStream());
-
-    if (aInput.EndOfStream())
-        throw LispErrCommentToEndOfFile();
-
-    if (aInput.Peek() == '/')
-    {
-      aInput.Next();  // consume /
-      goto REDO;
-    }
-    goto FALSEALARM;
-  }
-  else if (c == '/' && aInput.Peek() == '/')
-  {
-    aInput.Next(); //consume /
-    while (aInput.Next() != '\n' && !aInput.EndOfStream());
-    goto REDO;
-  }
-  // parse literal strings
-  else if (c == '\"')
-  {
-    std::string str;
-    str.push_back(c);
-    while (aInput.Peek() != '\"')
-    {
-      if (aInput.Peek() == '\\')
-      {
-        aInput.Next();
-
+    // skip whitespaces and comments
+    for (;;) {
+        // End of stream: return empty string
         if (aInput.EndOfStream())
-            throw LispErrParsingInput();
-      }
-      str.push_back(aInput.Next());
+            return aHashTable.LookUp("");
 
-      if (aInput.EndOfStream())
-          throw LispErrParsingInput();
-    }
-    str.push_back(aInput.Next()); // consume the close quote
-    return aHashTable.LookUp(str);
-  }
-  //parse atoms
-  else if (IsAlpha(c))
-  {
-    while (IsAlNum( aInput.Peek()))
-    {
-      aInput.Next();
-    }
-  }
+        c = aInput.Next();
 
-  else if (IsSymbolic(c))
-  {
-    while (IsSymbolic( aInput.Peek()))
-    {
-      aInput.Next();
-    }
-  }
-  else if (c == '_')
-  {
-    while (aInput.Peek() == '_')
-    {
-      aInput.Next();
-    }
-  }
-  else if (std::isdigit(c) || c == '.')
-  {
-    while (std::isdigit( aInput.Peek())) aInput.Next();
-    if (aInput.Peek() == '.')
-    {
-      aInput.Next();
-      while (std::isdigit( aInput.Peek())) aInput.Next();
-    }
-    if (aInput.Peek() == 'e' || aInput.Peek() == 'E') {
-        aInput.Next();
-        if (aInput.Peek() == '-' || aInput.Peek() == '+')
+        if (std::isspace(c))
+            continue;
+
+        // parse comments
+        if (c == '/' && aInput.Peek() == '*') {
             aInput.Next();
-        while (std::isdigit( aInput.Peek()))
-            aInput.Next();
-    }
-  }
-  // Treat the char as a space.
-  else
-  {
-    goto REDO;
-  }
+            for (;;) {
+                while (aInput.Next() != '*' && !aInput.EndOfStream())
+                    ;
 
-FINISH:
-  return aHashTable.LookUp(std::string(&aInput.StartPtr()[firstpos],aInput.Position()-firstpos));
+                if (aInput.EndOfStream())
+                    throw LispErrCommentToEndOfFile();
+
+                if (aInput.Peek() == '/') {
+                    aInput.Next();
+                    break;
+                }
+            }
+
+            continue;
+        }
+
+        if (c == '/' && aInput.Peek() == '/') {
+            aInput.Next();
+            while (aInput.Next() != '\n' && !aInput.EndOfStream())
+                ;
+            continue;
+        }
+
+        break;
+    }
+
+    // parse brackets
+    if (c == '(' || c == ')' || c == '{' || c == '}' || c == '[' || c == ']')
+        return aHashTable.LookUp(std::string(1, c));
+
+    // percent
+    if (c == '%')
+        return aHashTable.LookUp(std::string(1, c));
+
+    // comma and semicolon
+    if (c == ',' || c == ';')
+        return aHashTable.LookUp(std::string(1, c));
+
+    // parse . or ..
+    if (c == '.' && !std::isdigit(aInput.Peek())) {
+        std::string token;
+        token.push_back(c);
+        while (aInput.Peek() == '.')
+            token.push_back(aInput.Next());
+        return aHashTable.LookUp(token);
+    }
+
+    // parse literal strings
+    if (c == '\"') {
+        std::string str;
+        str.push_back(c);
+        while (aInput.Peek() != '\"') {
+            if (aInput.Peek() == '\\') {
+                aInput.Next();
+
+                if (aInput.EndOfStream())
+                    throw LispErrParsingInput();
+            }
+            str.push_back(aInput.Next());
+
+            if (aInput.EndOfStream())
+                throw LispErrParsingInput();
+        }
+        str.push_back(aInput.Next());
+        return aHashTable.LookUp(str);
+    }
+
+    // parse atoms
+    if (IsAlpha(c)) {
+        std::string atom;
+        atom.push_back(c);
+        while (IsAlNum(aInput.Peek()))
+            atom.push_back(aInput.Next());
+        return aHashTable.LookUp(atom);
+    }
+
+    // parse operators
+    if (IsSymbolic(c)) {
+        std::string op;
+        op.push_back(c);
+        while (IsSymbolic(aInput.Peek()))
+            op.push_back(aInput.Next());
+        return aHashTable.LookUp(op);
+    }
+
+    // parse subscripts
+    if (c == '_') {
+        std::string token;
+        token.push_back(c);
+        while (aInput.Peek() == '_')
+            token.push_back(aInput.Next());
+        return aHashTable.LookUp(token);
+    }
+
+    // parse numbers
+    if (std::isdigit(c) || c == '.') {
+        std::string number;
+        number.push_back(c);
+
+        while (std::isdigit(aInput.Peek()))
+            number.push_back(aInput.Next());
+
+        if (aInput.Peek() == '.') {
+            number.push_back(aInput.Next());
+            while (std::isdigit(aInput.Peek()))
+                number.push_back(aInput.Next());
+        }
+
+        if (aInput.Peek() == 'e' || aInput.Peek() == 'E') {
+            number.push_back(aInput.Next());
+            if (aInput.Peek() == '-' || aInput.Peek() == '+')
+                number.push_back(aInput.Next());
+            while (std::isdigit(aInput.Peek()))
+                number.push_back(aInput.Next());
+        }
+
+        return aHashTable.LookUp(number);
+    }
+
+    throw InvalidToken();
 }
