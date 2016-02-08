3265002f17f0fdccdb49be8f7b02228745e48df9
Grzegorz Mazur <teoretyk@gmail.com>
2016-02-08 14:39:08 +0100

    remove java applet Task #160 - remove java applet

--- JavaYacas/net/sf/yacas/TResult.java
+++
@@ -1,9 +0,0 @@
-package net.sf.yacas;
-
-
-interface TResult
-{
-  public int NrLines(YacasGraphicsContext  aGraphicsContext, int width) ;
-  public void draw(YacasGraphicsContext  aGraphicsContext, int current_word, int width, int height, int red, int green, int blue);
-};
-
--- JavaYacas/net/sf/yacas/TWord.java
+++
@@ -1,7 +0,0 @@
-package net.sf.yacas;
-
-class TWord
-{
-  public String word = new String();
-  public String digits = new String();
-};
