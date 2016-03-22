176c28e8381199f6ee39de29caa27591ff84c6dd
Grzegorz Mazur <teoretyk@gmail.com>
2016-03-22 08:57:18 +0100

    minor cleanup

--- include/yacas/genericstructs.h
+++ include/yacas/genericstructs.h
@@ -22,10 +22,10 @@ class GenericStruct : public GenericClass, NonCopyable
 {
 public:
   GenericStruct(LispChar * aTypeName, void* aData, void (*aDestructor)(void*));
-  virtual ~GenericStruct();
-  virtual const LispChar * TypeName() const override;
+  ~GenericStruct();
+  const LispChar * TypeName() const override;
   inline void* Data() {return iData;}
-
+private:
   void* iData;
   LispChar * iTypeName;
   void (*iDestructor)(void* data);
