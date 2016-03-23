6c9f51bb703a81fd4224ac9e7dbe2481a97bf046
Grzegorz Mazur <teoretyk@gmail.com>
2016-03-23 16:24:57 +0100

    fix freebsd build see http://svnweb.freebsd.org/ports/head/math/yacas/files/

--- src/mathcommands.cpp
+++ src/mathcommands.cpp
@@ -22,6 +22,7 @@
 #include "yacas/arggetter.h"
 #include "yacas/string_utils.h"
 
+#include <string>
 #include <cstring>
 #include <limits.h>
 #include <stdlib.h>
--- src/yacasmain.cpp
+++ src/yacasmain.cpp
@@ -72,6 +72,7 @@
 #if defined (__FreeBSD__) || defined (__DragonFly__)
 #include <stddef.h>
 #include <unistd.h>
+#include <sys/syslimits.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
 #endif
