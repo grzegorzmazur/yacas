eee25d7f71ce7d7554880d449e2c67ffe2066781
Grzegorz Mazur <teoretyk@gmail.com>
2016-06-03 16:03:28 +0200

    fix https://travis-ci.org/grzegorzmazur/yacas/jobs/135025175

--- cyacas/libyacas/src/lispeval.cpp
+++ cyacas/libyacas/src/lispeval.cpp
@@ -146,7 +146,7 @@ void ShowExpression(LispString& outString, LispEnvironment& aEnvironment,
   infixprinter.Print(aExpression, stream, aEnvironment);
   outString.append(stream.str());
 
-  std::regex_replace(outString, std::regex("(^\")|([^\\\\]\")"), static_cast<const char*>("\\\""));
+  std::regex_replace(outString, std::regex("(^\")|([^\\\\]\")"), std::string("\\\""));
 }
 
 static void TraceShowExpression(LispEnvironment& aEnvironment,
