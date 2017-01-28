c04c114808a3de0b44782a2ff462d955c039ad12
Grzegorz Mazur <teoretyk@gmail.com>
2017-01-29 00:30:30 +0100

    prepare source tarball containing external js libs Task #229 - yacas fails to build on openSUSE Build Service

---
+++ cyacas/packaging/rpm/prepare_tgz_for_obs.sh
@@ -0,0 +1,14 @@
+#/usr/bin/bash
+
+YACAS_VERSION=1.6.1
+
+wget https://github.com/grzegorzmazur/yacas/archive/v${YACAS_VERSION}.tar.gz
+tar xzf v${YACAS_VERSION}.tar.gz
+cd yacas-${YACAS_VERSION}/cyacas/yacas-gui/resources
+mkdir external_packages
+for u in `grep URL CMakeLists.txt | sed  "s/ *URL *//"`; do wget $u; mv `basename $u` external_packages; done
+mv CMakeLists.txt CMakeLists.txt.orig
+sed "s@https://github.com/.*/archive@\$\{CMAKE_CURRENT_SOURCE_DIR\}/external_packages@" < CMakeLists.txt.orig > CMakeLists.txt
+cd ../../../..
+tar czf yacas-${YACAS_VERSION}-obs.tar.gz yacas-${YACAS_VERSION}
+rm -rf yacas-${YACAS_VERSION}
