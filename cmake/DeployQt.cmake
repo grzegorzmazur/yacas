4e7da1a881ec16e101a49a5ae0a00ec205d6f4dd
Grzegorz Mazur <teoretyk@gmail.com>
2019-05-14 19:14:20 +0200

    make windeployqt more verbose

--- cmake/DeployQt.cmake
+++ cmake/DeployQt.cmake
@@ -45,7 +45,6 @@ function(windeployqt target)
     add_custom_command(TARGET ${target} POST_BUILD
         COMMAND "${CMAKE_COMMAND}" -E
             env PATH="${_qt_bin_dir}" "${WINDEPLOYQT_EXECUTABLE}"
-                --verbose 0
                 --no-compiler-runtime
                 --no-angle
                 --no-opengl-sw
