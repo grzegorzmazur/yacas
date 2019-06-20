6591f3f22ee32f8f147d029be01f94b534edd929
Grzegorz Mazur <teoretyk@gmail.com>
2019-06-20 20:48:14 +0200

    fix copyright headers in yacas_kernel

--- cyacas/yacas-kernel/src/yacas_engine.cpp
+++ cyacas/yacas-kernel/src/yacas_engine.cpp
@@ -1,17 +1,17 @@
 /*
- * This file is part of yacas_kernel.
- * Yacas_kernel is free software: you can redistribute it and/or modify
+ * This file is part of yacas.
+ * Yacas is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesset General Public License as
  * published by the Free Software Foundation, either version 2.1
  * of the License, or (at your option) any later version.
  *
- * Yacas_kernel is distributed in the hope that it will be useful,
+ * Yacas is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with yacas_kernel.  If not, see <http://www.gnu.org/licenses/>.
+ * License along with yacas. If not, see <http://www.gnu.org/licenses/>.
  *
  */
 
